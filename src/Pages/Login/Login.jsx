858f1f1d750609662b3332d319c0d962c8340348
Mariana Santos <154370339+MarianaToja@users.noreply.github.com>
2025-02-13 15:01:43 -0300

    update

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './login.css'
+
+ 
+
+export default function Login() {
+    return (
+        <>
+            <div className="background">
+                <br />
+                <form className='formularioLogin'>
+                    <h2>Login</h2>
+                    <div className="form-group">
+                        <label htmlFor="crm">CRM:</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            id="crm"
+                            placeholder="Informe o número da sua CRM" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Senha:</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            placeholder="Informe a sua senha" />
+                    </div>
+                    <button type="submit" className="botaoEntrar">Entrar</button>
+                </form>
+            </div>
+
+        </>
+    )
+}
