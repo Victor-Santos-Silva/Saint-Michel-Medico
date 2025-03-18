6434b703bcafbad6511131fc067e662b74039840
Felipe Paulino <154370191+oFelipeee@users.noreply.github.com>
2025-03-18 14:49:36 -0300

    Update/historio de pacientes, ajustando o design

--- src/Pages/Historico/Historico.jsx
+++ src/Pages/Historico/Historico.jsx
@@ -1,22 +1,101 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
+import './historico.css';
 
 const Historico = ({ nome, dia, hora, convenio, plano, especialidade, descricao }) => {
   return (
     <>
       <Header />
-      <div className="consulta-card">
-        <div className="foto"></div>
-        <div className="info">
-          <h2>{nome}</h2>
-          
-          <p><strong>Dia da consulta:</strong> {dia}</p>
-          <p><strong>Hora da consulta:</strong> {hora}</p>
-          <p><strong>Convênio:</strong> {convenio}</p>
-          <p><strong>Plano do Convênio:</strong> {plano}</p>
-          <p><strong>Especialidade:</strong> {especialidade}</p>
-          <p><strong>Descrição:</strong> {descricao}</p>
+      <div className="historico-container">
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
+        </div>
+
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
+        </div>
+
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
+        </div>
+
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
+        </div>
+
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
+        </div>
+
+        <div className="consulta-card">
+          <div className="foto">
+            <img src="src/Img/pacienteM.png" alt="Paciente" />
+          </div>
+          <div className="info">
+            <h2>{nome}</h2>
+            <p><strong>Dia da consulta:</strong> {dia}</p>
+            <p><strong>Hora da consulta:</strong> {hora}</p>
+            <p><strong>Convênio:</strong> {convenio}</p>
+            <p><strong>Plano do Convênio:</strong> {plano}</p>
+            <p><strong>Especialidade:</strong> {especialidade}</p>
+            <p><strong>Descrição:</strong> {descricao}</p>
+          </div>
         </div>
       </div>
       <Footer />
