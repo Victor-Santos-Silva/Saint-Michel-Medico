59a1ede5c3b031571fbb30dfb36619f2de6b4fd7
Victor dos Santos <victorsantos.goal@gmail.com>
2025-03-20 21:28:06 -0300

    puxando informaçoes do paciente

--- src/Pages/Home/HomeMedico.jsx
+++ src/Pages/Home/HomeMedico.jsx
@@ -4,12 +4,13 @@ import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import { useAuth } from '../../context/AuthContext'; // Importe o useAuth
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function HomeMedico() {
   const [data, setData] = useState([]);
   const { id } = useAuth(); // Use o hook useAuth para acessar o ID do médico logado
   const medicoLogadoId = id; // Armazena o ID do médico logado
-  
+
   useEffect(() => {
     axios.get(`http://localhost:5000/agendamento/listar?medico_id=${medicoLogadoId}`)
       .then(response => {
@@ -18,7 +19,6 @@ export default function HomeMedico() {
           agendamento => agendamento.medico_id === parseInt(medicoLogadoId) // Converte para número, se necessário
         );
         setData(agendamentosDoMedico);
-        console.log(agendamentosDoMedico);
       })
       .catch(error => {
         console.error("Erro ao buscar dados:", error); // Exibe o erro no console
@@ -35,19 +35,20 @@ export default function HomeMedico() {
           <div>
             <h2 className='titulo'>Consultas </h2>
           </div>
-          <div>
+          <div className='listaConsultas'>
             {data.length > 0 ? (
               data.map((item, index) => (
-                <div key={index}>
-                  <p>ID: {item.id}</p>
-                  <p>Usuário ID: {item.usuario_id}</p>
-                  <p>Médico ID: {item.medico_id}</p>
+                <div key={index} className='consultaItem'>
+                  {/* <img src={item.Usuario.imagemGenero} alt="" /> */}
+                  {/* <p>{item.Usuario.imagemGenero}</p> */}
+                  <p>{item.Usuario.nomeCompleto}</p>
                   <p>Data: {item.data}</p>
                   <p>Hora: {item.hora}</p>
+                  <Link className='button-prontuario' to='/prontuario'> Prontuario</Link>
                 </div>
               ))
             ) : (
-              <p>Nenhum paciente encontrado.</p>
+              <p className='semPacientes'>Nenhum paciente encontrado.</p>
             )}
           </div>
           <br />
