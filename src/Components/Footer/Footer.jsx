8449bca4262e71b3c51f2bc50c5b41643d308268
Victor dos Santos <victorsantos.goal@gmail.com>
2025-05-28 03:30:29 -0300

    colocando as redes sociais no footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,40 +6,33 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer className='footerMedico'>
-            <img 
-                src="../img/FooterLogo.png" 
-                alt="Logo do Hospital Saint-Michel" 
-                className='img-footeer' 
+            <img
+                src="../img/FooterLogo.png"
+                alt="Logo do Hospital Saint-Michel"
+                className='img-footeer'
             />
 
             <p>© 2025 Direitos reservados Hospital Saint-Michel by PNTEC-LTD</p>
 
             <div>
-                <span>Parcerias estratégicas:</span> 
+                <span>Parcerias estratégicas:</span>
                 <strong>Libbs</strong>
                 <span style={{ fontSize: '0.8rem', opacity: 0.8 }}>Saúde e Inovação</span>
             </div>
 
             <div className="footer-social">
-                <a 
-                    href="https://linkedin.com" 
-                    target="_blank" 
+                <a
+                    href="https://www.linkedin.com/in/saint-michael-hospital-47ab05359/"
+                    target="_blank"
                     rel="noopener noreferrer"
                     aria-label="LinkedIn"
                 >
                     <FaLinkedin />
                 </a>
-                <a 
-                    href="https://facebook.com" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    aria-label="Facebook"
-                >
-                    <FaFacebook />
-                </a>
-                <a 
-                    href="https://instagram.com" 
-                    target="_blank" 
+
+                <a
+                    href="https://www.instagram.com/hospital.saintmichel/"
+                    target="_blank"
                     rel="noopener noreferrer"
                     aria-label="Instagram"
                 >
